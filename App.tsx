dc8207b4f3ff0317a6eed8178aa0c107bfbf0d6b
Shubham Mahaseth <mahasethshubham@gmail.com>
2022-11-20 14:18:56 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  console.log('hello shubham');
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;

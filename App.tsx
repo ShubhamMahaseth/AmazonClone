b938f71bd8e987d16467904328e7d7fbdda9e507
Shubham Mahaseth <mahasethshubham@gmail.com>
2022-11-20 17:45:07 +0530

    Added react-native vector icon

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   console.log('hello shubham');
   return (
     <View>
+      <AntDesign name="customerservice" size={30} color="#900" />
       <Text>Hello</Text>
     </View>
   );
